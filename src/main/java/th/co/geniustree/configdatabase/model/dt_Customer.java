0fc36019712579b91ed5c981c11a4818ddda94f3
BestKung <best.kittiporn@gmail.com>
2015-05-27 16:07:15 +0700

    commit customer

---
+++ src/main/java/th/co/geniustree/configdatabase/model/dt_Customer.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.co.geniustree.configdatabase.model;
+
+import java.io.Serializable;
+import static java.util.Calendar.DATE;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import org.omg.CORBA.DATA_CONVERSION;
+
+/**
+ *
+ * @author Best
+ */
+@Entity
+public class dt_Customer implements Serializable{
+    @Id
+    private Integer pid;
+    private String hn;
+    private String name_th;
+    private String name_en;
+    private Date birthday;
+    private Integer age;
+    private String sex;
+    private String blood;
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getHn() {
+        return hn;
+    }
+
+    public void setHn(String hn) {
+        this.hn = hn;
+    }
+
+    public String getName_th() {
+        return name_th;
+    }
+
+    public void setName_th(String name_th) {
+        this.name_th = name_th;
+    }
+
+    public String getName_en() {
+        return name_en;
+    }
+
+    public void setName_en(String name_en) {
+        this.name_en = name_en;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.pid);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final dt_Customer other = (dt_Customer) obj;
+        if (!Objects.equals(this.pid, other.pid)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
