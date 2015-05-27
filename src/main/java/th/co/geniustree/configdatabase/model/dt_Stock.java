92b4569c14b3426b450c9394512b91a3b5722fbf
metavin007 <meta.vin@hotmail.com>
2015-05-27 16:20:15 +0700

    two commit

---
+++ src/main/java/th/co/geniustree/configdatabase/model/dt_Stock.java
@@ -0,0 +1,109 @@
+package th.co.geniustree.configdatabase.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class dt_Stock implements Serializable {
+
+    @Id
+    private String id;
+    private String type;
+    private String name;
+    private double costprice;
+    private double sellprice;
+    private double qty;
+    private double balances;
+    private Date expiration_date;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final dt_Stock other = (dt_Stock) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCostprice() {
+        return costprice;
+    }
+
+    public void setCostprice(double costprice) {
+        this.costprice = costprice;
+    }
+
+    public double getSellprice() {
+        return sellprice;
+    }
+
+    public void setSellprice(double sellprice) {
+        this.sellprice = sellprice;
+    }
+
+    public double getQty() {
+        return qty;
+    }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
+
+    public double getBalances() {
+        return balances;
+    }
+
+    public void setBalances(double balances) {
+        this.balances = balances;
+    }
+
+    public Date getExpiration_date() {
+        return expiration_date;
+    }
+
+    public void setExpiration_date(Date expiration_date) {
+        this.expiration_date = expiration_date;
+    }
+    
+}
